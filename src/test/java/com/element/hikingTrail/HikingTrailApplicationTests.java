646fea27f113c9abec2466dba6125dcfb53c18ae
Lucas Viola <lucasmatzenbacher@gmail.com>
2021-04-30 14:49:22 -0300

    Scaffolding project with Spring Boot

---
+++ src/test/java/com/element/hikingTrail/HikingTrailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.element.hikingTrail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HikingTrailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
